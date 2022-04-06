ba010bfc39791fe001ae4c37d3e0b380192b0d23
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-04-06 01:16:28 -0400

    Added movie-api and and changed home page to show top 10 movies

---
+++ apps/react-app/src/pages/HomePage/useMovies.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Movie } from './Movie';
+
+/**
+ * Hook to fetch movies
+ */
+export const useMovies = () => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState<unknown>();
+  const [movies, setMovies] = useState<Array<Movie>>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${apiUrl}/top-10-movies`);
+
+        if (!response.ok) {
+          const message = `Error: ${response.status}`;
+          throw new Error(message);
+        }
+
+        const movies = await response.json();
+        setMovies(movies);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, [apiUrl]);
+  return { isLoading, isError, error, movies };
+};
